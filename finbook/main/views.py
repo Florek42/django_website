5d78748a6594e744872809a146e6fb96352b6dfb
Krzysztof F <krzysztoff0423@gmail.com>
2024-01-21 21:31:06 +0100

    ffasf

---
+++ finbook/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def login(request):
+    return render(request, 'main/login.html')
+
+def register(request):
+    return render(request, 'main/register.html')
